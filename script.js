fc045ede208030ebec0891f8e5be4ca6dfb3044d
crankyshorts <51765840+crankyshorts@users.noreply.github.com>
2023-05-24 01:06:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-var houses = [];
-
+// Get the houses available for sale in Dallas/Fort Worth under $300k.
 function getHouses() {
+  // Make an API request to Zillow.
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.zillow.com/v3/homes/list/?city=Dallas&state=TX&maxPrice=300000&rentzestimate=true&limit=100&sort=price:asc");
+  xhr.open("GET", "https://www.zillow.com/homes/for_sale/Dallas-TX_rb/300000_price/");
   xhr.onload = function() {
-    if (xhr.status === 200) {
-      var housesData = JSON.parse(xhr.responseText);
-      housesData.results.forEach(function(house) {
-        houses.push(house);
-      });
-      updateHouses();
+    // Parse the JSON response.
+    var houses = JSON.parse(xhr.responseText);
+
+    // Loop through the houses and create a new HTML element for each one.
+    for (var i = 0; i < houses.length; i++) {
+      var house = houses[i];
+
+      // Create a new `.house` element.
+      var houseElement = document.createElement("div");
+      houseElement.className = "house";
+
+      // Add the house's image to the element.
+      var imageElement = document.createElement("img");
+      imageElement.src = house.photo;
+      houseElement.appendChild(imageElement);
+
+      // Add the house's title to the element.
+      var titleElement = document.createElement("h2");
+      titleElement.textContent = house.title;
+      houseElement.appendChild(titleElement);
+
+      // Add the house's price to the element.
+      var priceElement = document.createElement("p");
+      priceElement.textContent = house.price;
+      houseElement.appendChild(priceElement);
+
+      // Add the house's address to the element.
+      var addressElement = document.createElement("p");
+      addressElement.textContent = house.address;
+      houseElement.appendChild(addressElement);
+
+      // Append the house element to the `#houses` div.
+      $("#houses").append(houseElement);
     }
   };
   xhr.send();
 }
 
-function updateHouses() {
-  var housesList = document.getElementById("houses");
-  housesList.innerHTML = "";
-  houses.forEach(function(house) {
-    var houseLi = document.createElement("li");
-    var houseH2 = document.createElement("h2");
-    houseH2.textContent = house.address;
-    houseLi.appendChild(houseH2);
-    var houseP = document.createElement("p");
-    houseP.textContent = house.price + "\$";
-    houseLi.appendChild(houseP);
-    housesList.appendChild(houseLi);
-  });
-}
-
-window.onload = function() {
-  getHouses();
-  setInterval(getHouses, 3600000); // Update houses every hour
-};
+// Set up the interval to get hourly updates.
+setInterval(getHouses, 3600000);
